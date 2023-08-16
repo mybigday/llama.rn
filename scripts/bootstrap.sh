18326947355453a6d4f3bf272d4a657ff3320ffd
jhen <developer@jhen.me>
2023-08-16 09:09:46 +0800

    feat: sync llama.cpp

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,7 +10,7 @@ cp ./llama.cpp/ggml-metal.m ./cpp/ggml-metal.m
 cp ./llama.cpp/ggml-metal.metal ./cpp/ggml-metal.metal
 cp ./llama.cpp/ggml-alloc.h ./cpp/ggml-alloc.h
 cp ./llama.cpp/ggml-alloc.c ./cpp/ggml-alloc.c
-cp ./llama.cpp/llama-util.h ./cpp/llama-util.h 
+cp ./llama.cpp/llama-util.h ./cpp/llama-util.h
 cp ./llama.cpp/llama.h ./cpp/llama.h
 cp ./llama.cpp/llama.cpp ./cpp/llama.cpp
 cp ./llama.cpp/k_quants.h ./cpp/k_quants.h
@@ -53,5 +53,4 @@ echo "Replacement completed successfully!"
 yarn example
 
 # Apply patch
-patch -p0 -d ./cpp < ./scripts/llama.cpp.patch
 patch -p0 -d ./cpp < ./scripts/ggml-metal.m.patch
