b5a2c804425360f143e7c37614e9c72982616774
Jhen <developer@jhen.me>
2023-09-04 11:22:21 +0800

    chore(android): reuse code between new/old arch

--- android/src/newarch/java/com/rnllama/RNLlamaModule.java
+++ android/src/newarch/java/com/rnllama/RNLlamaModule.java
@@ -1,19 +1,12 @@
 package com.rnllama;
 
 import androidx.annotation.NonNull;
-import android.util.Log;
-import android.os.Build;
-import android.os.Handler;
-import android.os.AsyncTask;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableArray;
-import com.facebook.react.bridge.WritableMap;
-import com.facebook.react.bridge.Arguments;
 import com.facebook.react.module.annotations.ReactModule;
 
 import java.util.HashMap;
@@ -23,15 +16,14 @@ import java.io.FileInputStream;
 import java.io.PushbackInputStream;
 
 @ReactModule(name = RNLlamaModule.NAME)
-public class RNLlamaModule extends NativeRNLlamaSpec implements LifecycleEventListener {
+public class RNLlamaModule extends NativeRNLlamaSpec {
   public static final String NAME = "RNLlama";
 
-  private ReactApplicationContext reactContext;
+  private RNLlama rnllama = null;
 
   public RNLlamaModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    reactContext.addLifecycleEventListener(this);
-    this.reactContext = reactContext;
+    rnllama = new RNLlama(reactContext);
   }
 
   @Override
@@ -40,282 +32,48 @@ public class RNLlamaModule extends NativeRNLlamaSpec implements LifecycleEventLi
     return NAME;
   }
 
-  private HashMap<Integer, LlamaContext> contexts = new HashMap<>();
-
-  private int llamaContextLimit = 1;
-
   @ReactMethod
   public void setContextLimit(double limit, Promise promise) {
-    llamaContextLimit = (int) limit;
-    promise.resolve(null);
+    rnllama.setContextLimit(limit, promise);
   }
 
   @ReactMethod
   public void initContext(final ReadableMap params, final Promise promise) {
-    new AsyncTask<Void, Void, WritableMap>() {
-      private Exception exception;
-
-      @Override
-      protected WritableMap doInBackground(Void... voids) {
-        try {
-          int id = Math.abs(new Random().nextInt());
-          LlamaContext llamaContext = new LlamaContext(id, reactContext, params);
-          if (llamaContext.getContext() == 0) {
-            throw new Exception("Failed to initialize context");
-          }
-          contexts.put(id, llamaContext);
-          WritableMap result = Arguments.createMap();
-          result.putInt("contextId", id);
-          result.putBoolean("gpu", false);
-          result.putString("reasonNoGPU", "Currently not supported");
-          return result;
-        } catch (Exception e) {
-          exception = e;
-          return null;
-        }
-      }
-
-      @Override
-      protected void onPostExecute(WritableMap id) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(id);
-      }
-    }.execute();
+    rnllama.initContext(params, promise);
   }
 
   @ReactMethod
   public void completion(double id, final ReadableMap params, final Promise promise) {
-    final int contextId = (int) id;
-    new AsyncTask<Void, Void, WritableMap>() {
-      private Exception exception;
-
-      @Override
-      protected WritableMap doInBackground(Void... voids) {
-        try {
-          LlamaContext context = contexts.get(contextId);
-          if (context == null) {
-            throw new Exception("Context not found");
-          }
-          if (context.isPredicting()) {
-            throw new Exception("Context is busy");
-          }
-          WritableMap result = context.completion(params);
-          return result;
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(WritableMap result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(result);
-      }
-    }.execute();
+    rnllama.completion(id, params, promise);
   }
 
   @ReactMethod
   public void stopCompletion(double id, final Promise promise) {
-    final int contextId = (int) id;
-    new AsyncTask<Void, Void, Void>() {
-      private Exception exception;
-
-      @Override
-      protected Void doInBackground(Void... voids) {
-        try {
-          LlamaContext context = contexts.get(contextId);
-          if (context == null) {
-            throw new Exception("Context not found");
-          }
-          context.stopCompletion();
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(Void result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(result);
-      }
-    }.execute();
+    rnllama.stopCompletion(id, promise);
   }
 
   @ReactMethod
   public void tokenize(double id, final String text, final Promise promise) {
-    final int contextId = (int) id;
-    new AsyncTask<Void, Void, WritableMap>() {
-      private Exception exception;
-
-      @Override
-      protected WritableMap doInBackground(Void... voids) {
-        try {
-          LlamaContext context = contexts.get(contextId);
-          if (context == null) {
-            throw new Exception("Context not found");
-          }
-          return context.tokenize(text);
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(WritableMap result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(result);
-      }
-    }.execute();
+    rnllama.tokenize(id, text, promise);
   }
 
   @ReactMethod
   public void detokenize(double id, final ReadableArray tokens, final Promise promise) {
-    final int contextId = (int) id;
-    new AsyncTask<Void, Void, String>() {
-      private Exception exception;
-
-      @Override
-      protected String doInBackground(Void... voids) {
-        try {
-          LlamaContext context = contexts.get(contextId);
-          if (context == null) {
-            throw new Exception("Context not found");
-          }
-          return context.detokenize(tokens);
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(String result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(result);
-      }
-    }.execute();
+    rnllama.detokenize(id, tokens, promise);
   }
 
   @ReactMethod
   public void embedding(double id, final String text, final Promise promise) {
-    final int contextId = (int) id;
-    new AsyncTask<Void, Void, WritableMap>() {
-      private Exception exception;
-
-      @Override
-      protected WritableMap doInBackground(Void... voids) {
-        try {
-          LlamaContext context = contexts.get(contextId);
-          if (context == null) {
-            throw new Exception("Context not found");
-          }
-          return context.embedding(text);
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(WritableMap result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(result);
-      }
-    }.execute();
+    rnllama.embedding(id, text, promise);
   }
 
   @ReactMethod
   public void releaseContext(double id, Promise promise) {
-    final int contextId = (int) id;
-    new AsyncTask<Void, Void, Void>() {
-      private Exception exception;
-
-      @Override
-      protected Void doInBackground(Void... voids) {
-        try {
-          LlamaContext context = contexts.get(contextId);
-          if (context == null) {
-            throw new Exception("Context " + id + " not found");
-          }
-          context.release();
-          contexts.remove(contextId);
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(Void result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(null);
-      }
-    }.execute();
+    rnllama.releaseContext(id, promise);
   }
 
   @ReactMethod
   public void releaseAllContexts(Promise promise) {
-    new AsyncTask<Void, Void, Void>() {
-      private Exception exception;
-
-      @Override
-      protected Void doInBackground(Void... voids) {
-        try {
-          onHostDestroy();
-        } catch (Exception e) {
-          exception = e;
-        }
-        return null;
-      }
-
-      @Override
-      protected void onPostExecute(Void result) {
-        if (exception != null) {
-          promise.reject(exception);
-          return;
-        }
-        promise.resolve(null);
-      }
-    }.execute();
-  }
-
-  @Override
-  public void onHostResume() {
-  }
-
-  @Override
-  public void onHostPause() {
-  }
-
-  @Override
-  public void onHostDestroy() {
-    for (LlamaContext context : contexts.values()) {
-      context.release();
-    }
-    contexts.clear();
+    rnllama.releaseAllContexts(promise);
   }
 }
