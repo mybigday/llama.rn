8973d9445365020d2b17c687e8eed9a2de607eca
Jhen-Jie Hong <developer@jhen.me>
2025-01-24 09:57:00 +0800

    chore: format

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -1,47 +1,46 @@
 #!/bin/bash
 
-
 function cp_headers() {
-    mkdir -p ../ios/rnllama.xcframework/$1/rnllama.framework/Headers
-    cp ../cpp/*.h ../ios/rnllama.xcframework/$1/rnllama.framework/Headers/
+  mkdir -p ../ios/rnllama.xcframework/$1/rnllama.framework/Headers
+  cp ../cpp/*.h ../ios/rnllama.xcframework/$1/rnllama.framework/Headers/
 }
 
 function build_framework() {
-    # Parameters:
-    # $1: system_name (iOS/tvOS)
-    # $2: architectures
-    # $3: sysroot
-    # $4: output_path
-    # $5: build_dir
+  # Parameters:
+  # $1: system_name (iOS/tvOS)
+  # $2: architectures
+  # $3: sysroot
+  # $4: output_path
+  # $5: build_dir
 
-    cd $5
+  cd $5
 
-    # Configure CMake
-    cmake ../ios \
-        -GXcode \
-        -DCMAKE_SYSTEM_NAME=$1 \
-        -DCMAKE_OSX_ARCHITECTURES="$2" \
-        -DCMAKE_OSX_SYSROOT=$3 \
-        -DCMAKE_INSTALL_PREFIX=`pwd`/install \
-        -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
-        -DCMAKE_IOS_INSTALL_COMBINED=YES
+  # Configure CMake
+  cmake ../ios \
+    -GXcode \
+    -DCMAKE_SYSTEM_NAME=$1 \
+    -DCMAKE_OSX_ARCHITECTURES="$2" \
+    -DCMAKE_OSX_SYSROOT=$3 \
+    -DCMAKE_INSTALL_PREFIX=`pwd`/install \
+    -DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO \
+    -DCMAKE_IOS_INSTALL_COMBINED=YES
 
-    # Build
-    cmake --build . --config Release
+  # Build
+  cmake --build . --config Release
 
-    # Setup framework directory
-    rm -rf ../ios/rnllama.xcframework/$4
-    mkdir -p ../ios/rnllama.xcframework/$4
-    mv Release-$3/rnllama.framework ../ios/rnllama.xcframework/$4/rnllama.framework
-    mkdir -p ../ios/rnllama.xcframework/$4/rnllama.framework/Headers
+  # Setup framework directory
+  rm -rf ../ios/rnllama.xcframework/$4
+  mkdir -p ../ios/rnllama.xcframework/$4
+  mv Release-$3/rnllama.framework ../ios/rnllama.xcframework/$4/rnllama.framework
+  mkdir -p ../ios/rnllama.xcframework/$4/rnllama.framework/Headers
 
-    # Copy headers and metallib
-    cp_headers $4
+  # Copy headers and metallib
+  cp_headers $4
 
-    # TODO: May need to re-build metallib for tvOS
-    cp ../cpp/ggml-llama.metallib ../ios/rnllama.xcframework/$4/rnllama.framework/ggml-llama.metallib
+  # TODO: May need to re-build metallib for tvOS
+  cp ../cpp/ggml-llama.metallib ../ios/rnllama.xcframework/$4/rnllama.framework/ggml-llama.metallib
 
-    rm -rf ./*
+  rm -rf ./*
 }
 
 rm -rf build-ios
