984edf8c99c3af64246b8a3711342dcefdb769e3
Jhen-Jie Hong <developer@jhen.me>
2025-07-26 09:51:32 +0800

    feat(mock): update

--- jest/mock.js
+++ jest/mock.js
@@ -57,6 +57,7 @@ if (!NativeModules.RNLlama) {
         audio_tokens: [
           1000, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010,
         ],
+        content: '*giggles*',
         text: '*giggles*',
         completion_probabilities: [
           {
