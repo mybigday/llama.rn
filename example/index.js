5363cd826067d2c14c4275eabd56955522f49a43
Jhen-Jie Hong <developer@jhen.me>
2025-10-10 18:49:10 +0800

    fix: import of CustomEvent

--- example/index.js
+++ example/index.js
@@ -1,15 +1,13 @@
 import { AppRegistry } from 'react-native'
-// import NativeCustomEvent from 'react-native/Libraries/Events/CustomEvent'
+import NativeCustomEvent from 'react-native/src/private/webapis/dom/events/CustomEvent'
 import App from './src/App.tsx'
 import { name as appName } from './app.json'
 
-console.log(window.CustomEvent)
-
 // Setup for `mcp-sdk-client-ssejs` package
-// window.CustomEvent = class CustomEvent extends NativeCustomEvent {
-//   constructor(type, eventInitDict = {}) {
-//     super(type, eventInitDict)
-//   }
-// }
+window.CustomEvent = class CustomEvent extends NativeCustomEvent {
+  constructor(type, eventInitDict = {}) {
+    super(type, eventInitDict)
+  }
+}
 
 AppRegistry.registerComponent(appName, () => App)
