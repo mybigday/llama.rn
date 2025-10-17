d31f783153541ee38c9a06e0a44721b81c69f7d0
Jhen-Jie Hong <developer@jhen.me>
2025-10-17 20:18:31 +0800

    feat(android): make prebuilt libs for rnllama only

--- android/src/main/java/com/rnllama/LlamaContext.java
+++ android/src/main/java/com/rnllama/LlamaContext.java
@@ -557,34 +557,34 @@ public class LlamaContext {
 
     if (LlamaContext.isArm64V8a()) {
       if (hasDotProd && hasI8mm && hasAdreno) {
-        Log.d(NAME, "Loading librnllama_v8_2_dotprod_i8mm_opencl.so");
-        System.loadLibrary("rnllama_v8_2_dotprod_i8mm_opencl");
-        loadedLibrary = "rnllama_v8_2_dotprod_i8mm_opencl";
+        Log.d(NAME, "Loading librnllama_jni_v8_2_dotprod_i8mm_opencl.so");
+        System.loadLibrary("rnllama_jni_v8_2_dotprod_i8mm_opencl");
+        loadedLibrary = "rnllama_jni_v8_2_dotprod_i8mm_opencl";
       } else if (hasDotProd && hasI8mm) {
-        Log.d(NAME, "Loading librnllama_v8_2_dotprod_i8mm.so");
-        System.loadLibrary("rnllama_v8_2_dotprod_i8mm");
-        loadedLibrary = "rnllama_v8_2_dotprod_i8mm";
+        Log.d(NAME, "Loading librnllama_jni_v8_2_dotprod_i8mm.so");
+        System.loadLibrary("rnllama_jni_v8_2_dotprod_i8mm");
+        loadedLibrary = "rnllama_jni_v8_2_dotprod_i8mm";
       } else if (hasDotProd) {
-        Log.d(NAME, "Loading librnllama_v8_2_dotprod.so");
-        System.loadLibrary("rnllama_v8_2_dotprod");
-        loadedLibrary = "rnllama_v8_2_dotprod";
+        Log.d(NAME, "Loading librnllama_jni_v8_2_dotprod.so");
+        System.loadLibrary("rnllama_jni_v8_2_dotprod");
+        loadedLibrary = "rnllama_jni_v8_2_dotprod";
       } else if (hasI8mm) {
-        Log.d(NAME, "Loading librnllama_v8_2_i8mm.so");
-        System.loadLibrary("rnllama_v8_2_i8mm");
-        loadedLibrary = "rnllama_v8_2_i8mm";
+        Log.d(NAME, "Loading librnllama_jni_v8_2_i8mm.so");
+        System.loadLibrary("rnllama_jni_v8_2_i8mm");
+        loadedLibrary = "rnllama_jni_v8_2_i8mm";
       } else if (hasFp16) {
-        Log.d(NAME, "Loading librnllama_v8_2.so");
-        System.loadLibrary("rnllama_v8_2");
-        loadedLibrary = "rnllama_v8_2";
+        Log.d(NAME, "Loading librnllama_jni_v8_2.so");
+        System.loadLibrary("rnllama_jni_v8_2");
+        loadedLibrary = "rnllama_jni_v8_2";
       } else {
-        Log.d(NAME, "Loading default librnllama_v8.so");
-        System.loadLibrary("rnllama_v8");
-        loadedLibrary = "rnllama_v8";
+        Log.d(NAME, "Loading default librnllama_jni_v8.so");
+        System.loadLibrary("rnllama_jni_v8");
+        loadedLibrary = "rnllama_jni_v8";
       }
     } else if (LlamaContext.isX86_64()) {
-      Log.d(NAME, "Loading librnllama_x86_64.so");
-      System.loadLibrary("rnllama_x86_64");
-      loadedLibrary = "rnllama_x86_64";
+      Log.d(NAME, "Loading librnllama_jni_x86_64.so");
+      System.loadLibrary("rnllama_jni_x86_64");
+      loadedLibrary = "rnllama_jni_x86_64";
     } else {
       Log.d(NAME, "ARM32 is not supported, skipping loading library");
     }
